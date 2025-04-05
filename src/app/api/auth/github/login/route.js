35e55abc3b868ef0c5b62475d1045050d3d1eec9
Raahim <127599591+Raahim2@users.noreply.github.com>
2025-04-05 12:28:31 +0530

    Update route.js

--- src/app/api/auth/github/login/route.js
+++ src/app/api/auth/github/login/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request) {
   const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
-  const REDIRECT_URI = `http://localhost:3001/api/auth/github/callback`;
+  const REDIRECT_URI = `http://gitmax.vercel.app/api/auth/github/callback`;
   const scope = 'read:user user:email';
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(scope)}&response_type=code`;
 
